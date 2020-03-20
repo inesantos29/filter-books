426a88371b12350ed6900ee6a02013db416a5834
inesantos29 <ines.arts@hotmail.com>
2020-03-20 00:39:22 +0000

    Created SearchBar component and filtered search

--- src/views/Books/Books.jsx
+++ src/views/Books/Books.jsx
@@ -1,18 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+import SearchBar from '../SearchBar/SearchBar';
 import BookList from './BookList';
+
+const REACT_APP_API_URL = "https://my-json-server.typicode.com/0plus1/CodingChallenge-react/books"; 
 
 function Books() {
 
-  const [data, setData] = useState([]);
-
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [filteredBooks, setFilteredBooks] = useState([]);
+ 
   useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get('https://my-json-server.typicode.com/0plus1/CodingChallenge-react/books')
-      setData(result.data);
-    };
-    fetchData();
+    setLoading(true);
+    axios
+      .get(REACT_APP_API_URL)
+      .then(res => {
+        setBooks(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
+
+  
+  useEffect(() => {
+    setFilteredBooks(
+      books.filter(book =>
+        book.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, books]);
+
+  if (loading) {
+    return <p>Loading books...</p>;
+  }
   
   return (
     <section id="books">
@@ -21,9 +46,12 @@ function Books() {
           <div className="col-lg-12 text-center">
             <h2>Books</h2>
             <hr className="line-dark" />
+            <SearchBar 
+              onChange={e => setSearch(e.target.value)} 
+            />
           </div>
         </div>
-        <BookList books={data}/>
+        <BookList books={filteredBooks}/>
       </div>
     </section> 
   )
---
+++ src/views/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Search} from "./style";
+
+
+const SearchInput = ({ onChange }) => (
+  <Search>
+    <div className="search pt-4 pb-4">
+      <input className="search__input" type="text" onChange={onChange} placeholder="Search here..."/>
+    </div>
+  </Search>
+  
+);
+
+export default SearchInput;
